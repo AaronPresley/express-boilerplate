3485640e3943479f685f25c1b3a27cfb2494afe0
Aaron Presley <ap@aaronpresley.net>
2019-07-26 14:01:34 -0700

    Now automatically doing lint fixes through webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,16 +3,16 @@ import * as CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
-export default ({
+export default {
   mode,
 
   entry: './src/client/index.tsx',
-  
+
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, '/dist'),
   },
-  
+
   devtool: 'source-map',
 
   resolve: {
@@ -23,16 +23,28 @@ export default ({
     rules: [
       { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+          quiet: true,
+        },
+      },
     ],
   },
 
   plugins: [
-    new CopyWebpackPlugin([{
-      context: path.join(__dirname, './src/client'),
-      from: 'index.html',
-    }, {
-      context: path.join(__dirname, './src/client'),
-      from: 'assets/**/*.*',
-    }]),
+    new CopyWebpackPlugin([
+      {
+        context: path.join(__dirname, './src/client'),
+        from: 'index.html',
+      },
+      {
+        context: path.join(__dirname, './src/client'),
+        from: 'assets/**/*.*',
+      },
+    ]),
   ],
-});
+};
