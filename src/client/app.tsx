bbdeba290edbdb109621bd5c15ed8ae625092b0f
Aaron Presley <ap@aaronpresley.net>
2019-08-17 20:19:42 -0700

    Removed old source from repo, removed normalize CSS

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 const App: React.FunctionComponent<{}> = (): JSX.Element => (
-  <div>
-    <Switch>
-      <Route component={(): JSX.Element => <>Hi</>} />
-    </Switch>
-  </div>
+  <Switch>
+    <Route component={(): JSX.Element => <>Hi</>} />
+  </Switch>
 );
 
 export default App;
